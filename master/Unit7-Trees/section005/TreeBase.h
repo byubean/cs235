55b82cf39e0e75cead9c6f3f15e96cb8e83505d0
Gordon Bean <gbean@cs.byu.edu>
2022-03-21 10:02:01 -0600

    Unit 7, 8

---
+++ master/Unit7-Trees/section005/TreeBase.h
@@ -0,0 +1,8 @@
+//
+// Created by Gordon Bean on 3/15/22.
+//
+
+#ifndef SECTION005_TREEBASE_H
+#define SECTION005_TREEBASE_H
+
+#endif //SECTION005_TREEBASE_H
---
+++ master/Unit7-Trees/section006/TreeBase.h
@@ -0,0 +1,8 @@
+//
+// Created by Gordon Bean on 3/15/22.
+//
+
+#ifndef SECTION006_TREEBASE_H
+#define SECTION006_TREEBASE_H
+
+#endif //SECTION006_TREEBASE_H
